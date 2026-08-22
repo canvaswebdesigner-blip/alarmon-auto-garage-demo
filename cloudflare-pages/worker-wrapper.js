6ca67b5596230a7562e6936d6713f50c1d4880c1
canvaswebdesigner-blip <canvaswebdesigner-blip@users.noreply.github.com>
2026-08-22 17:12:11 +0300

    Add Cloudflare Pages asset routing

---
+++ cloudflare-pages/worker-wrapper.js
@@ -0,0 +1,24 @@
+import app from "./worker/index.js";
+
+const staticPrefixes = ["/assets/", "/images/"];
+const staticFiles = new Set([
+  "/favicon.svg",
+  "/file.svg",
+  "/globe.svg",
+  "/window.svg",
+]);
+
+export default {
+  async fetch(request, env, context) {
+    const { pathname } = new URL(request.url);
+
+    if (
+      staticPrefixes.some((prefix) => pathname.startsWith(prefix)) ||
+      staticFiles.has(pathname)
+    ) {
+      return env.ASSETS.fetch(request);
+    }
+
+    return app.fetch(request, env, context);
+  },
+};
